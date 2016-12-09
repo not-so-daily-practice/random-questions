ebdf817a7c578c88ca565ba4f093f628253c7089
SZanlongo <SZanlongo@users.noreply.github.com>
2016-12-09 08:42:17 -0500

    Convert solid shapes on bitmap to outline

---
+++ outline_2d_bitmap.py
@@ -0,0 +1,34 @@
+import pprint
+
+pp = pprint.PrettyPrinter()
+
+bitmap = [[0, 0, 0, 0, 0, 0, 0, ],
+          [0, 1, 1, 1, 0, 1, 0],
+          [0, 1, 1, 1, 1, 1, 0],
+          [0, 1, 1, 1, 1, 0, 0],
+          [0, 0, 1, 1, 1, 0, 0],
+          [0, 1, 1, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0, 0, 0]]
+
+pp.pprint(bitmap)
+
+print("\n\n\n")
+
+for x, line in enumerate(bitmap):
+    for y, point in enumerate(line):
+        try:
+            if point in {1, 2} and \
+                            bitmap[x - 1][y] in {1, 2} and \
+                            bitmap[x + 1][y] in {1, 2} and \
+                            bitmap[x][y - 1] in {1, 2} and \
+                            bitmap[x][y + 1] in {1, 2}:
+                bitmap[x][y] = 2
+        except:
+            pass
+
+for x, line in enumerate(bitmap):
+    for y, point in enumerate(line):
+        if point == 2:
+            bitmap[x][y] = 0
+
+pp.pprint(bitmap)
